74853e1b86d1081498a5bf03de02f896906b0135
ivanatomic43 <ivana.tomic96@gmail.com>
2023-03-12 15:42:44 +0100

    Store update

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { employeesApi } from "./apis/employeesApi";
+import { tasksApi } from "./apis/tasksApi";
 
 export const store = configureStore({
   reducer: {
-    [employeesApi.reducerPath]: employeesApi.reducer
+    [employeesApi.reducerPath]: employeesApi.reducer,
+    [tasksApi.reducerPath]: tasksApi.reducer
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware()
        .concat(employeesApi.middleware)
+       .concat(tasksApi.middleware)
  }
 })
 
 export { useFetchEmployeesQuery, useAddEmployeeMutation, useDeleteEmployeeMutation} from './apis/employeesApi';
+export { useFetchTasksQuery, useAddTaskMutation } from './apis/tasksApi';
 
 
